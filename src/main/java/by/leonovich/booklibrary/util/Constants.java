704ff26d0c4f018fb0dc4788f10052b575a6cebc
CyberAlexander <taurum13@gmail.com>
2022-02-04 08:59:00 +0300

    Scanner Bean name moved to Constants

--- src/main/java/by/leonovich/booklibrary/util/Constants.java
+++ src/main/java/by/leonovich/booklibrary/util/Constants.java
@@ -1,17 +1,18 @@
 package by.leonovich.booklibrary.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by alexanderleonovich on 11.06.15.
  */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Constants {
     public static final String WRITE_TITLE = "write title of book: ";
     public static final String WRITE_AUTHOR = "write author of book: ";
     public static final String WRITE_YEAR = "write year of book: ";
     public static final String WRITE_ID = "Write id: ";
     public static final String FILE = "books.txt";
-    public static final String SPRING_SETTINGS = "spring.cfg.xml";
     public static final String BOOK_SERVICE_BEAN = "bookService";
-    public static final String BOOK_DAO_BEAN = "bookDao";
-    public static final String BOOK_BEAN = "book";
-
+    public static final String SCANNER_BEAN = "scanner";
 }
